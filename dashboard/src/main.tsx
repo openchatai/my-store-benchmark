f76413e451e69fd57d1cc754d7f3558c49a71fb5
ah7255703 <ah7255703@gmail.com>
2024-01-18 18:15:22 +0200

    Update React + TypeScript + Vite template and remove unused scripts

--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -4,21 +4,12 @@ import { MainShell } from './Shell'
 import "@fontsource-variable/montserrat"
 import './index.css';
 import { SWRConfig } from 'swr'
-import { ping } from './data';
 import { Toaster } from './@/ui/sonner';
 import { ThemeProvider } from "next-themes"
 
-const pinga = (await ping()).data === '200';
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ThemeProvider defaultTheme='light' themes={['light', 'dark']}>
-    <SWRConfig
-      value={{
-        isOnline() {
-          return pinga
-        },
-      }}
-    >
+    <SWRConfig>
       <MainShell>
         <App />
       </MainShell>
